b7534c4976c0ee9eecc37786514031cdd39b1309
itohdak <itohdak@gmail.com>
2021-07-24 16:10:18 +0000

    update memo

--- memo.sh
+++ memo.sh
@@ -3,30 +3,30 @@
 set -e
 
 # install docker
-sudo apt update
-sudo apt install -y \
-	apt-transport-https \
-	ca-certificates \
-	curl \
-	gnupg \
-	lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+# sudo apt update
+# sudo apt install -y \
+# 	apt-transport-https \
+# 	ca-certificates \
+# 	curl \
+# 	gnupg \
+# 	lsb-release
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# echo \
+#   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt update
+# sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # add $USER (isucon) to docker group
 # sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-docker run hello-world
+# sudo usermod -aG docker $USER
+# newgrp docker
+# docker run hello-world
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# docker-compose --version
 
 
 # install netdata
@@ -38,3 +38,13 @@ wget https://github.com/tkuchiki/alp/releases/download/v1.0.5/alp_linux_amd64.zi
 unzip alp_linux_amd64.zip
 sudo install ./alp /usr/local/bin
 rm -rf alp_linux_amd64.zip alp
+
+# install pt-query-digest
+sudo apt-get update
+sudo apt-get install -y wget gnupg2 lsb-release curl
+wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
+sudo dpkg -i percona-release_latest.generic_all.deb
+sudo apt-get update
+# if dpkg error occurs
+# sudo apt-get install --fix-broken
+rm percona-release_latest.generic_all.deb
