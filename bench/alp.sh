749782e2bbf4546101c692372e0e2e23d3ca1c84
itohdak <itohdak@gmail.com>
2021-07-24 16:10:45 +0000

    moved alp.sh

--- bench/alp.sh
+++ bench/alp.sh
@@ -1,5 +1,5 @@
 alp ltsv -r \
 	--sort sum \
-	--file=logs/nginx/access.log \
+	--file=../webapp/logs/nginx/access.log.back \
 	--filters 'Uri contains "/api/"' \
 	-m '/api/estate/[0-9]+,/api/chair/[0-9]+,/api/recommended_estate/[0-9]+,/api/estate/req_doc/[0-9]+,/api/chair/buy/[0-9]+'
